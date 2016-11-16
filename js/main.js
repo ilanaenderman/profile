adb0e6eb85c51cbe7810244507f1fa45e9fcd0cd
Ilana <ilanaenderman@hotmail.com>
2016-11-16 11:56:54 +0100

    created personal profile

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// drop down navbar ------------------------
+
+$('.dropdown-toggle').dropdown()
+console.log
+
+// Skill bar -------------------------------
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
